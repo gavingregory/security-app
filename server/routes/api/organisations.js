477cb0118abe871791e6bfc64c6944f440f05ed2
Geoff Whitehead <geoff1012@gmail.com>
2016-06-06 15:35:48 +0100

    updated schemas

--- server/routes/api/organisations.js
+++ server/routes/api/organisations.js
@@ -4,56 +4,24 @@ module.exports = function (express, passport) {
   var router = express.Router({ mergeParams: true });
   var orgRouter = express.Router({mergeParams: true});
 
-  /**
-   * @api {post} / Creates a new organisation.
-   * @apiName CreateOrganisation
-   * @apiGroup Organisations
-   *
-   * @apiExample Example usage:
-   *   endpoint: http://localhost:8080/api/v1/organisations
-   */
-  router.post('/', passport.authenticate('bearer', {session: false}), function (req, res) {
+  router.post('/', function (req, res) {
     return res.status(codes.not_implemented)
       .send({_errors: [{message: 'Not yet implemented.'}]});
   });
 
   router.use('/:org_id', orgRouter);
 
-  /**
-  * @api {get} / Gets your organisation details.
-  * @apiName GetOrganisation
-  * @apiGroup Organisations
-  *
-  * @apiExample Example usage:
-  *   endpoint: http://localhost:8080/api/v1/organisations
-  */
-  orgRouter.get('/', passport.authenticate('bearer', {session: false}), function (req, res) {
+  orgRouter.get('/', function (req, res) {
     return res.status(codes.not_implemented)
       .send({_errors: [{message: 'Not yet implemented.'}]});
   });
 
-  /**
-  * @api {put} / Updates your organisation details.
-  * @apiName UpdateOrganisation
-  * @apiGroup Organisations
-  *
-  * @apiExample Example usage:
-  *   endpoint: http://localhost:8080/api/v1/organisations
-  */
-  orgRouter.put('/', passport.authenticate('bearer', {session: false}), function (req, res) {
+  orgRouter.put('/', function (req, res) {
     return res.status(codes.not_implemented)
       .send({_errors: [{message: 'Not yet implemented.'}]});
   });
 
-  /**
-   * @api {delete} / Deletes your organisation.
-   * @apiName DeleteOrganisation
-   * @apiGroup Organisations
-   *
-   * @apiExample Example usage:
-   *   endpoint: http://localhost:8080/api/v1/organisations
-   */
-  orgRouter.delete('/', passport.authenticate('bearer', {session: false}), function (req, res) {
+  orgRouter.delete('/', function (req, res) {
     return res.status(codes.not_implemented)
       .send({_errors: [{message: 'Not yet implemented.'}]});
   });
