b6a5ab9749cf2adfda9e325e87837fe49cabefbb
Gavin Gregory <gavin.ian.gregory@gmail.com>
2016-06-09 20:08:33 +0100

    My organisation cleaned up on the angular app.

--- client/app/services/organisationFactory.js
+++ client/app/services/organisationFactory.js
@@ -4,14 +4,14 @@ angular.module('logApp')
       create: function (data) {
         return $http.post('api/v1/organisations', data);
       },
-      get: function (id) {
-        return $http.get('api/v1/organisations/' + id);
+      get: function () {
+        return $http.get('api/v1/organisations');
       },
       update: function (data) {
-        return $http.put('api/v1/organisations/' + data._id, data);
+        return $http.put('api/v1/organisations', data);
       },
-      delete: function (id) {
-        return $http.delete('api/v1/organisations/' + id);
+      delete: function () {
+        return $http.delete('api/v1/organisations');
       }
     };
   }]);
