9601265e5bb0ec135b093249d95ef0878979308d
Gavin Gregory <gavin.ian.gregory@gmail.com>
2016-06-21 22:12:19 +0100

    Added a comment list popup on the event list page.

--- client/app/controllers/events/EventListController.js
+++ client/app/controllers/events/EventListController.js
@@ -1,16 +1,55 @@
-angular.module('logApp')
-  .controller('EventListController', ['$scope', 'eventFactory', 'socket', function ($scope, eventFactory, socket) {
+(function(){
+  'use strict';
 
-    eventFactory.list().then(function(resp){
-      $scope.events = resp.data;
-    }). catch( function( err ){
-      console.log(err);
+angular
+  .module('app')
+  .controller('EventListController', [
+    '$mdPanel', 'eventFactory', EventListController
+  ]);
+
+  function EventListController($mdPanel, eventFactory) {
+    var vm = this;
+    vm.events = [];
+
+    eventFactory.list().then(function(res){
+      vm.events = [].concat(res.data);
+    })
+    .catch(function(res){
+      console.log(res);
     });
-    $scope.$on('socket:error', function (ev, data) {
-      console.log(ev);
-      console.log(data);
-    });
-    $scope.$on('socket:connection', function(socket){
-      console.log('a event user connected');
-    });
-  }]);
+
+    this.showDialog = function($event, row) {
+      console.log($event);
+      console.log(row);
+      var config = {
+        attachTo: angular.element(document.body),
+        controller: ['mdPanelRef', function (mdPanelRef) {
+          var vm = this;
+          this._mdPanelRef = mdPanelRef;
+          vm.closeDialog = function () {
+            this._mdPanelRef && this._mdPanelRef.close();
+          };
+        }],
+        controllerAs: 'vm',
+        locals: {
+          event: row
+        },
+        disableParentScroll: true,
+        templateUrl: 'app/views/events/_comments.html',
+        hasBackdrop: true,
+        panelClass: 'event-comment-dialog',
+        position: $mdPanel.newPanelPosition()
+          .absolute()
+          .center(),
+        trapFocus: true,
+        zIndex: 150,
+        clickOutsideToClose: true,
+        escapeToClose: true,
+        focusOnOpen: true
+      };
+      $mdPanel.open(config);
+    };
+
+
+  };
+})();
